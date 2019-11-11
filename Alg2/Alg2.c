ab4bb43770e2b7b95c3e22f505ddb25f5c21f229
Антон Кубанов <goodeee@mail.ru>
2019-11-11 15:29:39 +0300

    UTF8

--- Alg2/Alg2.c
+++ Alg2/Alg2.c
@@ -1,21 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
-//#include "Algr01.h"
-
+п»ї#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-//Кубанов Антон, Урок2
+//РљСѓР±Р°РЅРѕРІ РђРЅС‚РѕРЅ, РЈСЂРѕРє2
 int bin(int dec) {
 	if (dec > 0) {
 		int temp = (bin(dec /= 2)) * 10 + dec % 2;
 		return temp;
-
 	}
 	else return 0;
 
 }
-int main(void) {
+int main(void) {  
 	int n = 16;
-	printf("%d perevod v BIN %d\n", n, (bin(n) * 10) + n % 2);  // не понял, как вы рекурсию добавить первое деление
+	printf("%d perevod v BIN %d\n", n, (bin(n) * 10) + n % 2);  // РЅРµ РїРѕРЅСЏР», РєР°Рє РІ СЂРµРєСѓСЂСЃРёСЋ РґРѕР±Р°РІРёС‚СЊ РїРµСЂРІРѕРµ РґРµР»РµРЅРёРµ
 	system("pause");
 	return 0;
 
