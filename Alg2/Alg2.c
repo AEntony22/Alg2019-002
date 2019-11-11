a43ea3b0d72e42bd3e794150fc73053b36ca3524
Антон Кубанов <goodeee@mail.ru>
2019-11-11 19:20:54 +0300

    p.1 + pp2.1 2.2

--- Alg2/Alg2.c
+++ Alg2/Alg2.c
@@ -3,11 +3,6 @@
 #include <math.h>
 //Кубанов Антон, Урок2
 void solution1();
-void solution2();
-void solution3();
-void menu();
-
-
 int bin(int dec) {
 	if (dec > 0) {
 		int temp = (bin(dec /= 2)) * 10 + dec % 2;
@@ -16,6 +11,13 @@ int bin(int dec) {
 	else return 0;
 
 }
+
+
+void solution2();
+void solution3();
+void menu();
+
+
 int main(void) {
 	int sel = 0;
 	do
@@ -50,11 +52,40 @@ int main(void) {
 		int n = 16;
 		printf("%d perevod v BIN %d\n", n, (bin(n) * 10) + n % 2);  // не понял, как в рекурсию добавить первое деление
 	}
+	long power(int a, int b)
+	{
+		long p = 1;
+		while (b)
+		{
+			p = p * a;
+			b--;
+		}
+		return p;
+	}
 
+	long powerR (int a, int b)
+	{
+		
+			if (b == 0) {
+				return 1;
+			}else {
+				return (a*powerR(a, b - 1));
+			}
+			
+		}
+		
 	void solution2()
 	{
+		/*2. Реализовать функцию возведения числа a в степень b:
+           a. без рекурсии;
+           b. рекурсивно;
+           c. *рекурсивно, используя свойство чётности степени.*/
 		printf("Solution 2\n");
-		// Решение
+		
+	
+			printf("stepen' bez recursii %ld\n", power(3, 3));
+			printf("stepen' s recursii %ld\n", powerR(3, 3));
+		
 	}
 	void solution3()
 	{
